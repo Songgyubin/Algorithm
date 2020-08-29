b8320516d0dc8a83eb6546070d1a69092f2fc75f
songgyubin <thd0427__@naver.com>
2020-08-29 20:20:08 +0900

    string

--- programmers/StringCompression.kt
+++ programmers/StringCompression.kt
@@ -1,64 +1,29 @@
-package programmers
-
-import java.util.*
+package string
 
 private fun main() {
-//"aabbaccc"	7
-//"ababcdcdababcdcd"	9
-//"abcabcdede"	8
-//"abcabcabcabcdededededede"	14
-//"xababcdcdababcdcd"
-
-    println(solution("aabbaccc"))
-
-//    println(solution("abcabcabcabcdededededede")) // 14
-
-}
-
-data class Word(
-    val word : String,
-    var count : Int = 1
-)
-private fun solution(s: String): Int {
-    //"aabbaccc"	7
-    var answer = Int.MAX_VALUE
-    for(space in 1..s.length) {
-        val compressedWordList = LinkedList<Word>()
-        var startIndex = 0
-        var endIndex = 0
-        while(endIndex != s.length) {
-            endIndex = (startIndex + space).let {
-                if(it > s.length) s.length
-                else it
+    val s = readLine()!!
+    var answer = s.length
+    for (i in 1 until s.length) {
+        var j = 0
+        var com = String()
+        var sub = String()
+        while (j + i <= s.length) {
+            sub = s.substring(j, j + i)
+            var n = 1
+            j += i
+            while (j + i <= s.length) {
+                if (s.substring(j, j + i) != sub) {
+                    break
+                }
+                j += i
+                n++
             }
-            val currentWord = s.substring(startIndex, endIndex)
-
-            // 문자열 달라질 때
-            if(compressedWordList.isEmpty() || compressedWordList.peekLast().word != currentWord) compressedWordList.add(Word(currentWord))
-            // 중복 체크 count++
-            else compressedWordList.peekLast().count++
-            // 다음 인덱스 검사
-            startIndex = endIndex
+            com += if (n != 1) "(${n})$sub" else sub
         }
-
-        // fold : 초기값 0
-        // 누산기 개념
-        val length = compressedWordList.fold(0) {
-                acc, word ->
-            // if word.count 의 초기값은 1이므로 1이면 중복이 없다는 뜻
-            // else 문자열 길이 비교이므로 toString 후 length
-            acc + word.word.length + if(word.count == 1) 0 else {
-                word.count.toString().length
-            }
-        }
-        println("answer: $answer")
-        println("length: $length")
-
-        // 최소값 length중 최솟값 구하기 위해
-       if(length < answer) {
-
-            answer = length
-        }
+        com += s.substring(j)
+        answer = Math.min(answer, com.length)
     }
-    return answer
+
+    println(answer)
+
 }
