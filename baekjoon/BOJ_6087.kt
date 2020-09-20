88975c16caa72b8472af7584497d59274b01e8ed
songgyubin <thd0427__@naver.com>
2020-09-20 23:17:09 +0900

    bfs

--- baekjoon/BOJ_6087.kt
+++ baekjoon/BOJ_6087.kt
@@ -1,68 +1,64 @@
+package bfs
+
 import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.math.min
 
-// 방향
-val goX = arrayOf(0, 0, -1, 1)
-val goY = arrayOf(1, -1, 0, 0)
-private lateinit var arr: Array<Array<Char>>
-private lateinit var vis: Array<IntArray>
+private val goX = intArrayOf(-1, 1, 0, 0)
+private val goY = intArrayOf(0, 0, -1, 1)
 
-private var laser = ArrayList<Point>()
-private var answer = Int.MAX_VALUE
+private lateinit var arr: Array<CharArray>
+private lateinit var dis: Array<IntArray>
 
-private var w = 0
-private var h = 0
+private var startX = -1
+private var startY = -1
+
+private var endX = -1
+private var endY = -1
+
 
 private fun main() {
-    val s = readLine()!!.split(' ').map(String::toInt)
-    w = s[0]
-    h = s[1]
-    arr = Array(h) { Array(w) { ' ' } }
-    vis = Array(h) { IntArray(w) { Int.MAX_VALUE } }
 
+    val (w, h) = readLine()!!.split(' ').map(String::toInt)
+    arr = Array(h) { CharArray(w) }
+    dis = Array(h) { IntArray(w) { -1 } }
     for (i in 0 until h) {
         readLine()!!.forEachIndexed { j, c ->
             arr[i][j] = c
-            if (c == 'C') laser.add(Point(i, j, -1, 0))
+            if (c == 'C') {
+                if (startX == -1) {
+                    startX = i
+                    startY = j
+                } else {
+                    endX = i
+                    endY = j
+                }
+            }
         }
     }
-    bfs()
+    bfs(h, w)
+    println(dis[endX][endY] - 1)
 }
 
-private fun bfs() {
-    var queue: Queue<Point> = LinkedList()
-    var c1 = laser[0] // start
-    var c2 = laser[1] // end
-
-    queue.add(c1)
-    vis[c1.x][c1.y] = 0
+private fun bfs(h: Int, w: Int) {
+    val queue: Queue<Pair<Int, Int>> = LinkedList()
+    queue.add(Pair(startX, startY))
+    dis[startX][startY] = 0
 
     while (queue.isNotEmpty()) {
-        val x = queue.peek().x
-        val y = queue.peek().y
-        val dir = queue.peek().dir
-        val count = queue.peek().count
-        queue.poll()
-        if (x == c2.x && y == c2.y) {
-            answer = min(count, answer)
-        }
-        for (i in 0 until 4) {
-            val curX = x + goX[i]
-            val curY = y + goY[i]
-            val curDir = i // 방향
-            if (curX < 0 || curX >= h || curY < 0 || curY >= w || arr[curX][curY] == '*') continue
-            var tmp = count
-            if (dir != curDir && dir != -1) {
-                tmp++
-            }
-            if (vis[curX][curY] < tmp) continue
 
-            vis[curX][curY] = tmp
-            queue.add(Point(curX, curY, curDir, tmp))
+        val (x, y) = queue.poll()
+        for (i in 0 until 4) {
+            var curX = x + goX[i]
+            var curY = y + goY[i]
+            while (curX in 0 until h && curY in 0 until w) {
+                if (arr[curX][curY] == '*') break
+                if (dis[curX][curY] == -1) {
+                    dis[curX][curY] = dis[x][y] + 1
+                    queue.add(Pair(curX, curY))
+                }
+                curX += goX[i]
+                curY += goY[i]
+            }
         }
     }
-    println(answer)
-}
 
-data class Point(val x: Int, val y: Int, val dir: Int, val count: Int)
+}
