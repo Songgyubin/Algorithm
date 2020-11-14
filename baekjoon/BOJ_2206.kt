7dce8469f3cafb7db6af051a97ec91ff031ebff3
DESKTOP-UD2063R\thd04 <thd0427__@naver.com>
2020-11-15 01:05:27 +0900

    bfs

--- baekjoon/BOJ_2206.kt
+++ baekjoon/BOJ_2206.kt
@@ -1,7 +1,6 @@
-package bfs
-
 import java.util.*
 import kotlin.math.min
+import kotlin.system.exitProcess
 
 
 private val goX = arrayOf(0, 0, -1, 1)
@@ -28,53 +27,47 @@ private fun main() {
     }
     bfs()
 
-    if (answer == Int.MAX_VALUE) println(-1)
-    else println(answer)
+    println(-1)
 }
 
 private fun bfs() {
 
-    var queue: Queue<Node> = LinkedList()
+    val queue: Queue<Node> = LinkedList()
     queue.add(Node(0, 0, 1, 0))
     while (queue.isNotEmpty()) {
-        val curX = queue.peek().x
-        val curY = queue.peek().y
-        val count = queue.peek().count
-        val breakCount = queue.peek().breakCount
-        queue.poll()
+        val (curX, curY, count, breakCount) = queue.poll()
+        if (curX == n - 1 && curY == m - 1) {
+            println(count)
+            exitProcess(0)
+        }
         vis[breakCount][curX][curY] = true
-
         if (curX == n - 1 && curY == m - 1) {
             answer = min(count, answer)
         }
-
-
         for (k in 0 until 4) {
             val x = curX + goX[k]
             val y = curY + goY[k]
+            if (x < 0 || x >= n || y < 0 || y >= m) continue
 
-            if (x >= 0 && x < n && y >= 0 && y < m) {
-                if (breakCount == 1) {
-                    if (!vis[1][x][y] && arr[x][y] == 0) {
-                        queue.add(Node(x, y, count + 1, breakCount))
-                        vis[1][x][y] = true
-                    }
-                } else {
-                    if (arr[x][y] == 1) {
-                        if (!vis[1][x][y]) {
-                            queue.add(Node(x, y, count + 1, 1))
-                            vis[1][x][y] = true
-                        }
-                    } else if (arr[x][y] == 0) {
-                        if (!vis[0][x][y]) {
-                            vis[0][x][y] = true
-                            queue.add(Node(x, y, count + 1, 0))
-                        }
-                    }
-                }
+            if (breakCount == 1) {
+                if (vis[1][x][y] || arr[x][y] == 1) continue
 
+                queue.add(Node(x, y, count + 1, breakCount))
+                vis[1][x][y] = true
             }
+            else {
+                if (arr[x][y] == 1) {
+                    if (vis[1][x][y]) continue
+                    queue.add(Node(x, y, count + 1, 1))
+                    vis[1][x][y] = true
 
+                } else if (arr[x][y] == 0) {
+                    if (vis[0][x][y]) continue
+                    vis[0][x][y] = true
+                    queue.add(Node(x, y, count + 1, 0))
+
+                }
+            }
         }
 
 
