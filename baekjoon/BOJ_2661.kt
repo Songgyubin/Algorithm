9638c0f1556da380cbb84e92b1ee1696489f2548
DESKTOP-UD2063R\thd04 <thd0427__@naver.com>
2021-01-02 22:46:25 +0900

    backtracking

--- baekjoon/BOJ_2661.kt
+++ baekjoon/BOJ_2661.kt
@@ -1,36 +1,35 @@
-package backtracking
+package boj.backtracking
 
-import java.io.BufferedReader
-import java.io.InputStreamReader
+import kotlin.system.exitProcess
 
-fun test(str: StringBuilder): Boolean {
-    val lastIdx = str.length - 1
-    if (lastIdx == 0) return true
-    for (i in 0 until str.length / 2) {
-        if (str.substring(lastIdx - i..lastIdx)
-                .compareTo(str.substring(lastIdx - i - 1 - i until lastIdx - i)) == 0
-        )
-            return false
+private fun main() {
+    val n = readLine()!!.toInt()
+    if (n == 1) {
+        println(1)
+        return
     }
-    return true
+    backtracking("", n)
+
 }
 
-fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val N = readLine()!!.toInt()
-    var solved = false
-    fun dfs(dep: Int, str: StringBuilder) {
-        if (solved) return
-        if (dep == N) {
-            print(str)
-            solved = true
-            return
-        }
-        for (i in 1..3) {
-            str.append(i)
-            if (test(str))
-                dfs(dep + 1, str)
-            str.deleteCharAt(str.length - 1)
+private fun backtracking(s: String, n: Int) {
+    if (s.length == n) {
+        println(s)
+        exitProcess(0)
+    }
+    for (i in 1..3) {
+        if (isGood(s + i)) {
+            backtracking(s + i, n)
         }
     }
-    dfs(0, StringBuilder(""))
+}
+
+private fun isGood(s: String): Boolean {
+    var len = s.length / 2
+    for (i in 1..len) {
+        if (s.substring(s.length - i) == s.substring(s.length - 2 * i, s.length - i)) {
+            return false;
+        }
+    }
+    return true
 }
