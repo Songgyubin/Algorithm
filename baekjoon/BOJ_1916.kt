d963bee25dada9574541e5df321ee4e9fe280c54
DESKTOP-UD2063R\thd04 <thd0427__@naver.com>
2021-01-05 18:11:35 +0900

    dijkstra

--- baekjoon/BOJ_1916.kt
+++ baekjoon/BOJ_1916.kt
@@ -1,49 +1,52 @@
 package boj.dijkstra
 
-import boj.floydwarshall.INF
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.util.*
 import kotlin.collections.ArrayList
 
-private lateinit var arr: Array<ArrayList<Node1>>
+private lateinit var arr: Array<ArrayList<City>>
 private lateinit var dist: IntArray
-
-private val queue = PriorityQueue<Node1>()
-private lateinit var st: StringTokenizer
+private const val INF = 1000000000
 private fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-
-    val n = br.readLine().toInt()
-    val m = br.readLine().toInt()
+    val n = readLine()!!.toInt()
+    val m = readLine()!!.toInt()
     arr = Array(n + 1) { ArrayList() }
     dist = IntArray(n + 1) { INF }
     for (i in 0 until m) {
-        st = StringTokenizer(br.readLine())
-        arr[st.nextToken().toInt()].add(Node1(st.nextToken().toInt(), st.nextToken().toInt()))
+        val (a, b, c) = readLine()!!.split(' ').map { it.toInt() }
+        arr[a].add(City(b, c))
     }
-    st = StringTokenizer(br.readLine())
-    val (start, end) = intArrayOf(st.nextToken().toInt(), st.nextToken().toInt())
-    dijkstra(start)
+    val (start, end) = readLine()!!.split(' ').map { it.toInt() }
+    dijk(start)
     println(dist[end])
 }
 
-private fun dijkstra(start: Int) {
+private fun dijk(start: Int) {
+
     dist[start] = 0
-    queue.add(Node1(start, 0))
+
+    val queue: Queue<City> = LinkedList()
+
+    queue.add(City(start, 0))
+
     while (queue.isNotEmpty()) {
-        val (curIdx, curDist) = queue.poll()
-        if (dist[curIdx] < curDist) continue
-        for (i in 0 until  arr[curIdx].size) {
-            val (nextIdx, nextDist) = intArrayOf(arr[curIdx][i].index, curDist + arr[curIdx][i].dist)
-            if (nextDist < dist[nextIdx]) {
-                dist[nextIdx] = nextDist
-                queue.add(Node1(nextIdx, nextDist))
+        val (curIndex, curDist) = queue.poll()
+
+        if (dist[curIndex] < curDist) continue
+
+        for (i in 0 until arr[curIndex].size) {
+
+            var (nextIndex, nextDist) = arr[curIndex][i]
+            nextDist += curDist
+
+            if (nextDist < dist[nextIndex]) {
+                dist[nextIndex] = nextDist
+                queue.add(City(nextIndex, nextDist))
             }
         }
     }
+
 }
 
-private data class Node1(val index: Int, val dist: Int) : Comparable<Node1> {
-    override fun compareTo(other: Node1): Int = dist - other.dist
+private data class City(val index: Int, val dist: Int) : Comparable<City> {
+    override fun compareTo(other: City): Int = this.dist - other.dist
 }
