4adbdbdd5056a8f2eaab476b951311efc26b7937
DESKTOP-UD2063R\thd04 <thd0427__@naver.com>
2021-01-06 15:51:10 +0900

    lcs

--- baekjoon/BOJ_9251.kt
+++ baekjoon/BOJ_9251.kt
@@ -1,7 +1,20 @@
-package baekjoon.dp
+package boj
 
-private fun main(){
-    val a = CharArray(1000)
-    val b = CharArray(1000)
+import kotlin.math.max
 
+private fun main() {
+    val s = "0${readLine()!!}"
+    val ss = "0${readLine()!!}"
+    val dp = Array<IntArray>(1001) { IntArray(1001) }
+
+    for (i in 1 until ss.length) {
+        val tmp = ss[i]
+        for (j in 1 until s.length) {
+            if (tmp == s[j]) dp[i][j] = dp[i - 1][j - 1] + 1
+            else dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+        }
+    }
+    var answer = 0
+    dp.forEach { answer = max(answer, it.maxOrNull()!!) }
+    println(answer)
 }
