b70e9e97384e963e4a032bb30c339837384c9ae6
DESKTOP-UD2063R\thd04 <thd0427__@naver.com>
2021-03-01 20:56:28 +0900

    bfs

--- baekjoon/BOJ_5014.kt
+++ baekjoon/BOJ_5014.kt
@@ -1,38 +1,31 @@
-package bfs
+package boj.bfs
 
 import java.util.*
-import kotlin.system.exitProcess
-
 
 private fun main() {
-    val (F, S, G, U, D) = readLine()!!.split(' ').map(String::toInt)
-    if (S > F) {
-        println("use the stairs")
-        exitProcess(0)
-    }
-    val dist = Array<Int>(F + 1) { -1 }
-    dist[S] = 0
+
+    val (f, s, g, u, d) = readLine()!!.split(' ').map { it.toInt() }
+    //s 출발 g 도착
+    // 총 f
+    val dist = IntArray(f + 1) { -1 }
+    dist[s] = 0
 
     val queue: Queue<Int> = LinkedList()
 
-    queue.add(S)
+    queue.add(s)
     while (queue.isNotEmpty()) {
         val floor = queue.poll()
-
-        if (floor + U <= F && dist[floor + U] == -1) {
-            dist[floor + U] = dist[floor] + 1
-            queue.add(floor + U)
+        if (floor + u <= f && dist[floor + u] == -1) {
+            dist[floor + u] = dist[floor] + 1
+            queue.add(floor + u)
         }
-        if (floor - D > 0 && dist[floor - D] == -1) {
-            dist[floor - D] = dist[floor] + 1
-            queue.add(floor - D)
+        if (floor - d > 0 && dist[floor - d] == -1) {
+            dist[floor - d] = dist[floor] + 1
+            queue.add(floor - d)
         }
 
     }
-    if (dist[G] == -1) {
-        println("use the stairs")
-        exitProcess(0)
-    } else println(dist[G])
-
+    if (dist[g] == -1) println("use the stairs")
+    else println(dist[g])
 
 }
